9dfe1a94b6b783a656204773c2d8a3e0a77db890
ICT Layer <32593898+ICTLAYER@users.noreply.github.com>
2023-08-29 01:27:13 +0600

    Initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({})
+
+// const invoices = await Prisma.invoice.findMany({
+//   orderBy: {
+//     invoice_id: 'asc',
+//   },
+// })
+const invoices = await prisma.invoice.findMany({
+  orderBy: {
+    invoice_id: 'desc',
+  },
+})
+</script>
+
+<template>
+  <div v-for="inv in invoices" :key="inv.invoice_id">
+    <p>{{ inv.invoice_id }} || {{ inv.invoice_date }}</p>
+  </div>
+</template>
